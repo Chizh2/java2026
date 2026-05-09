1e61c02118c311dd833ec6364fc68fa27c4b6c0f
Chizh2 <2006ilja@gmail.com>
2026-05-09 15:31:13 +0300

    Практика 7
    пример с использованием var

---
+++ pract7.java
@@ -0,0 +1,12 @@
+public class pract7{
+    public static void main(String[] args) {
+        var a = 10;          // int
+        var b = 3.14;        // double
+        var c = "Hello";     // String
+        var d = true;        // boolean
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
